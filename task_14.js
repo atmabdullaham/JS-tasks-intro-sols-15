6dfe3cb82865f97b5ac1cf38ce50333c6f5869e7
atmabdullaham <atmabdullaham@gmail.com>
2024-11-09 15:57:40 +0600

    completed task 14 and 15

--- task_14.js
+++ task_14.js
@@ -1,5 +1,5 @@
 /*
-### Task-6
+### Task-14
 Your task is to divide the given number by 11 and show the remainder as the output.
 
 **Input:**
@@ -16,3 +16,19 @@ Print the remainder.
 **Sample Output:**
 9
 */
+
+const num1 = 501;
+const remainder1 = num1 % 11;
+console.log(remainder1);
+const num2 = 502;
+const remainder2 = num2 % 11;
+console.log(remainder2);
+const num3 = 503;
+const remainder3 = num3 % 11;
+console.log(remainder3);
+const num4 = 504;
+const remainder4 = num4 % 11;
+console.log(remainder4);
+const num5 = 505;
+const remainder5 = num5 % 11;
+console.log(remainder5);
--- task_15.js
+++ task_15.js
@@ -9,6 +9,12 @@ var a = isNaN(2-10);
 Explain your answers.
 */
 
+var a = isNaN('11');
+console.log(a);
+let b = isNaN(2 - 10);
+console.log(b);
+
+// Answer is false, its give the false when it a number or were able to covert to number. "11" and 2-10 is convertable to number and negative number so it's nota NUmber of Nan is false.
 
 /*
 ### Task-5
@@ -20,8 +26,10 @@ var b = isNaN(5 + "5");
 
 Explain your answers.
 */
-
-
+var c = isNaN("123");
+console.log(c);
+var d = isNaN(5 + "5");
+console.log(d);
 /*
 ### Task-6
 What will be the result of the following codes:
@@ -32,3 +40,8 @@ var x = isNaN("Hello");
 
 Explain your answers.
 */
+
+var x = isNaN("100");
+console.log(x);
+var y = isNaN("Hello");
+console.log(y);
